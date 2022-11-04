11d13225efe0c32645485a3e15f6a2c3160a1d37
alireza kargar <imanesche@gmail.com>
2022-11-04 17:21:01 +0330

    add swaggrt

--- app/Loaders/fastify.loader.ts
+++ app/Loaders/fastify.loader.ts
@@ -1,8 +1,49 @@
 import { FastifyInstance } from 'fastify';
 import mongoDB_plugin from './../Plugins/mongodb.plugin'
 import Routers_plugin from './../Plugins/routers.plugin'
+import fastifySwagger from '@fastify/swagger'
 
 export default (fastifyApp: FastifyInstance) => {
     fastifyApp.register(mongoDB_plugin)
     fastifyApp.register(Routers_plugin)
+    fastifyApp.register(fastifySwagger, {
+        swagger: {
+            info: {
+                title: 'Test swagger',
+                description: 'Testing the Fastify swagger API',
+                version: '0.1.0'
+            },
+            externalDocs: {
+                url: 'https://swagger.io',
+                description: 'Find more info here'
+            },
+            host: 'localhost',
+            schemes: ['http'],
+            consumes: ['application/json'],
+            produces: ['application/json'],
+            tags: [
+                { name: 'user', description: 'User related end-points' },
+                { name: 'code', description: 'Code related end-points' }
+            ],
+            definitions: {
+                User: {
+                    type: 'object',
+                    required: ['id', 'email'],
+                    properties: {
+                        id: { type: 'string', format: 'uuid' },
+                        firstName: { type: 'string' },
+                        lastName: { type: 'string' },
+                        email: { type: 'string', format: 'email' }
+                    }
+                }
+            },
+            securityDefinitions: {
+                apiKey: {
+                    type: 'apiKey',
+                    name: 'apiKey',
+                    in: 'header'
+                }
+            }
+        }
+    })
 }
--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import loaders from './app/Loaders';
 
 const fastify: FastifyInstance = Fastify()
 
-const start = () => {
+const start = async () => {
     try {
         loaders(fastify)
+        await fastify.ready()
+        fastify.swagger()
         fastify.listen({ port: 3000 }, (err) => {
             if (err) console.log(err)
             else console.log(`app listen on port 3000`)

37c99c9bb72608125c68bd32757568c029dc7595
alireza kargar <imanesche@gmail.com>
2022-11-05 01:50:16 +0330

    read env files in root folders

--- index.ts
+++ index.ts
@@ -1,23 +1,26 @@
 import Fastify, { FastifyInstance } from 'fastify';
-import loaders from './app/Loaders';
+require('dotenv').config({ path: '.env' })
+import loaders from './app/Loaders'
 
 const fastify: FastifyInstance = Fastify()
 
 const start = async () => {
     try {
+        const port: number = Number(process.env.PORT) || 3000
+
         loaders(fastify)
 
         await fastify.ready()
         // fastify.ready((err) => {
         //     if (err) throw err
         // })
-        
+
         fastify.swagger()
-        fastify.listen({ port: 3000 }, (err) => {
+        fastify.listen({ port: port }, (err) => {
             if (err) console.log(err)
             else {
-                console.log(`app listen on port 3000`)
-                console.log(`http://127.0.0.1:3000/documentation`)
+                console.log(`app listen on port ${port}`)
+                console.log(`http://127.0.0.1:${port}/documentation`)
             }
         })
 

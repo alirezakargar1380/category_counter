00d92f5ab458794d3eb07594031120a59f6fc709
alireza kargar <imanesche@gmail.com>
2022-11-05 12:11:37 +0330

    added test

--- test/test.ts
+++ test/test.ts
@@ -1,1 +1,9 @@
-import {} from "mocha"
+import {it} from "mocha"
+import {expect} from "chai"
+import * as categorysService from "./../app/Services/category.services"
+
+describe("CATEGORYS SERVICE", () => {
+    it('create', () => {
+        expect(categorysService.create('', 1)).to.equal(true)
+    })
+})

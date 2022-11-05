615223a87c92018256995fab7c3cdf0714045482
alireza kargar <imanesche@gmail.com>
2022-11-05 13:04:05 +0330

    added mongoose loader

--- app/Loaders/index.ts
+++ app/Loaders/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify';
 import fastifyLoader from './fastify.loader';
+import mongooseLoader from "./mongoose.loader"
 
 export default (fastify: FastifyInstance) => {
     fastifyLoader(fastify)
+    mongooseLoader();
 }
--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,30 @@
-import {it} from "mocha"
-import {expect} from "chai"
+import { it } from "mocha"
+import { expect } from "chai"
 import * as categorysService from "./../app/Services/category.services"
+import * as counterService from "./../app/Services/counter.service"
+import mongooseLoader from "./../app/Loaders/mongoose.loader"
+
+// connect to mongoDB
+mongooseLoader()
 
 describe("CATEGORYS SERVICE", () => {
-    it('create', () => {
-        expect(categorysService.create('', 1)).to.equal(true)
+    
+
+    it('create', (done) => {
+        categorysService
+            .create("test title")
+            .then(() => {
+                done()
+            })
+            .catch((err) => {
+                done(err)
+            })
+    })
+
+})
+
+describe("COUNTER SERVICE", () => {
+    it('create', (done) => {
+        counterService.create().then(() => { done() }).catch((err) => { done(err) })
     })
 })

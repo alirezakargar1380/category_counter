42876eff72a141417474aa58ddc7a59ecac760b0
alireza kargar <imanesche@gmail.com>
2022-11-05 17:41:20 +0330

    added aggOne to repository

--- app/Repositorys/categorys.repo.ts
+++ app/Repositorys/categorys.repo.ts
@@ -24,6 +24,30 @@ export class categorysRepository {
                     "preserveNullAndEmptyArrays": true
                 }
             }
-        ])
+        ]).skip(0).limit(1)
+    }
+
+    async aggOne() {
+        return await this.categoryModel.aggregate([
+            {
+                $lookup: {
+                    from: "counters",
+                    localField: "_id",
+                    foreignField: "category_id",
+                    as: "counter"
+                }
+            },
+            {
+                $match: {
+                    $expr: { $eq: ['$_id', { $toObjectId: "6366279cc654fbf47abbfd36" }] },
+                }
+            },
+            {
+                $unwind: {
+                    "path": "$counter",
+                    "preserveNullAndEmptyArrays": true
+                }
+            }
+        ]).skip(0).limit(1)
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -3,6 +3,7 @@ import { expect } from "chai"
 import * as categorysService from "./../app/Services/category.services"
 import * as counterService from "./../app/Services/counter.service"
 import mongooseLoader from "./../app/Loaders/mongoose.loader"
+import { repositorys } from "./../app/Repositorys"
 
 // connect to mongoDB
 mongooseLoader()
@@ -27,4 +28,6 @@ describe("COUNTER SERVICE", () => {
     it('create', (done) => {
         counterService.create().then(() => { done() }).catch((err) => { done(err) })
     })
+
+    repositorys.categorys.aggOne().then((d) => console.log(d))
 })

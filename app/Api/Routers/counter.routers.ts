0d722fe645ba3c6bd91952a6d25cc1f3b7ee9d99
alireza kargar <imanesche@gmail.com>
2022-11-06 16:00:09 +0330

    Update counter.routers.ts

--- app/Api/Routers/counter.routers.ts
+++ app/Api/Routers/counter.routers.ts
@@ -1,8 +1,15 @@
 import { FastifyInstance } from 'fastify';
 import * as counterOpts from '../Schemas/counter.options'
 import {plus, min} from "./../Controllers/counter.controller";
+import { loginMiddleware } from './../Middlewares/login.middleware';
 
 export default (fastifyApp: FastifyInstance) => {
-    fastifyApp.put('/counter/plus/:category_id', { schema: counterOpts.plusCounterOpts }, plus)
-    fastifyApp.put('/counter/min/:category_id', { schema: counterOpts.minCounterOpts }, min)
+    fastifyApp.put('/counter/plus/:category_id', { 
+        schema: counterOpts.plusCounterOpts,
+        preHandler: loginMiddleware
+    }, plus)
+    fastifyApp.put('/counter/min/:category_id', { 
+        schema: counterOpts.minCounterOpts,
+        preHandler: loginMiddleware
+    }, min)
 }

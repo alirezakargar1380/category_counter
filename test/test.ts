dbe691829e11f12f2569774b2664baf27419baec
alireza kargar <imanesche@gmail.com>
2022-11-06 15:51:05 +0330

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -4,17 +4,20 @@ import * as categorysService from "./../app/Services/category.services"
 import * as counterService from "./../app/Services/counter.service"
 import mongooseLoader from "./../app/Loaders/mongoose.loader"
 import { repositorys } from "./../app/Repositorys"
+import { ObjectId } from "@fastify/mongodb"
 
 // connect to mongoDB
 mongooseLoader()
 
+let category: any
+
 describe("CATEGORYS SERVICE", () => {
-    
 
     it('create', (done) => {
         categorysService
             .create("test title")
-            .then(() => {
+            .then((data) => {
+                category = data
                 done()
             })
             .catch((err) => {
@@ -22,10 +25,28 @@ describe("CATEGORYS SERVICE", () => {
             })
     })
 
+    // it('get category by page number', (done) => {
+
+    // })
+
 })
 
 describe("COUNTER SERVICE", () => {
+    it('create counter', (done) => {
+        counterService.create(category._id, 1000).then(() => { done() }).catch((err) => { done(err) })
+    })
+
     it('min', (done) => {
-        counterService.min("6366279cc654fbf47abbfd36").then(() => { done() }).catch((err) => { done(err) })
+        counterService.min(category._id as string).then((counter) => {
+            expect(counter.counter_number).to.equal(999)
+            done()
+        }).catch((err) => { done(err) })
+    })
+
+    it('plus', (done) => {
+        counterService.plus(category._id as string).then((counter) => {
+            expect(counter.counter_number).to.equal(1000)
+            done()
+        }).catch((err) => { done(err) })
     })
 })

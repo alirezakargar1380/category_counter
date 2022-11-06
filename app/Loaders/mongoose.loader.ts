d8d684bb1679cee7b4e5b127db4f00ace92e62b2
alireza kargar <imanesche@gmail.com>
2022-11-06 16:02:01 +0330

    added env example

--- app/Loaders/mongoose.loader.ts
+++ app/Loaders/mongoose.loader.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default async () => {
   try {
-    const url: string = 'mongodb://127.0.0.1:27017/testYu';
+    const url: string = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/testCounterDB';
     await mongoose.connect(url, {});
   } catch (e) {
     throw e;
